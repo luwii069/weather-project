80ce9809c9e53ca76eb191dc5c3196135fbb8b40
Mulaa <louismulaa06@gmail.com>
2024-04-10 16:27:35 +0300

    linked js to html and re started the API

---
+++ index.js
@@ -0,0 +1,12 @@
+const apikey = "70a55350ee2f091cde8b4e74c9fe47a3";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=Nairobi ";
+async function checkWeather(){
+const response = await fetch(apiUrl+`&apikey=${apikey}`)
+let data = await response.json();
+console.log(data);
+}
+
+
+
+
+checkWeather();
