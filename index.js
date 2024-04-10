35f488988f4f3bfa08ea1491dda0898ec20be246
Mulaa <louismulaa06@gmail.com>
2024-04-10 16:56:52 +0300

    added the units for each value

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let data = await response.json();
 console.log(data);
 
 document.querySelector(".city").innerHTML = data.name 
-document.querySelector(".temp").innerHTML = data.main.temp 
-document.querySelector(".humidity").innerHTML = data.main.humidity
-document.querySelector(".wind").innerHTML = data.wind.speed
+document.querySelector(".temp").innerHTML = data.main.temp + "°C"
+document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
+document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr"
 }
 checkWeather();
