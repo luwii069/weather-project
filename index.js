3a6057833c4b26a9acd0d70dd98bfbd2533ab220
Mulaa <louismulaa06@gmail.com>
2024-04-10 17:05:55 +0300

    made the  search box active

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const apikey = "70a55350ee2f091cde8b4e74c9fe47a3";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=Nairobi ";
-async function checkWeather(){
-const response = await fetch(apiUrl+`&apikey=${apikey}`)
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q= ";
+const searchbox=doment.querySelector(".search input")
+const searchtn=doment.querySelector(".search button")
+async function checkWeather(city){
+const response = await fetch(apiUrl+ city +`&apikey=${apikey}`)
 let data = await response.json();
 console.log(data);
 
@@ -10,4 +12,8 @@ document.querySelector(".temp").innerHTML =map.round (data.main.temp )+ "°C"
 document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
 document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr"
 }
-checkWeather();
+
+searchtn.addEventListener("click", ()=>{
+    checkWeather()
+})
+;
