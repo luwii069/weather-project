6b3486469dde94760e16a4ac0782d810be7c9d44
Mulaa <louismulaa06@gmail.com>
2024-04-10 16:54:06 +0300

    succesfully displayed the data for nrb city

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ async function checkWeather(){
 const response = await fetch(apiUrl+`&apikey=${apikey}`)
 let data = await response.json();
 console.log(data);
-}
+
 document.querySelector(".city").innerHTML = data.name 
 document.querySelector(".temp").innerHTML = data.main.temp 
 document.querySelector(".humidity").innerHTML = data.main.humidity
 document.querySelector(".wind").innerHTML = data.wind.speed
-
+}
 checkWeather();
